7a1957a65ef6647f6f078a3d8accd8d434ba6af2
flada-auxv <aseknock@gmail.com>
2023-01-31 18:16:00 +0900

    Respond to multiple PINGs

--- app/server.go
+++ app/server.go
@@ -10,30 +10,27 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	defer l.Close()
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
 
+	defer l.Close()
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	for {
 		s := bufio.NewScanner(conn)
 		s.Scan()
 		if err = s.Err(); err != nil {
 			fmt.Println("Error while reading from connection", err.Error())
 			os.Exit(1)
 		}
+		fmt.Println("Received message:", s.Text())
 
 		// FIXME: Just sending "PONG" back in RESP format
 		w := bufio.NewWriter(conn)
