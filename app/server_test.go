7cc1a8fdc331bb18128735130d68d4c131b588ef
flada-auxv <aseknock@gmail.com>
2023-02-05 13:46:46 +0900

    :lipstick: redundant

--- app/server_test.go
+++ app/server_test.go
@@ -18,9 +18,9 @@ func TestParse(t *testing.T) {
 			name: "hi",
 			args: args{[]byte("*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n")},
 			want: []RESP{
-				RESP{ Type: '*', Count: 2, Data: []byte("$4\r\nECHO\r\n$3\r\nhey\r\n"), Raw: []byte("*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n")},
-				RESP{ Type: '$', Count: 4, Data: []byte("ECHO"), Raw: []byte("$4\r\nECHO\r\n")},
-				RESP{ Type: '$', Count: 3, Data: []byte("hey"), Raw: []byte("$3\r\nhey\r\n")},
+				{Type: '*', Count: 2, Data: []byte("$4\r\nECHO\r\n$3\r\nhey\r\n"), Raw: []byte("*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n")},
+				{Type: '$', Count: 4, Data: []byte("ECHO"), Raw: []byte("$4\r\nECHO\r\n")},
+				{Type: '$', Count: 3, Data: []byte("hey"), Raw: []byte("$3\r\nhey\r\n")},
 			},
 		},
 	}
