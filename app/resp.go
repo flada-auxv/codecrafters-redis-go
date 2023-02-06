240b27f199be0fa7a08e481787aa0ecbf42d1d73
flada-auxv <aseknock@gmail.com>
2023-02-06 23:06:57 +0900

    no for loop in Parse(). simply assume that only one Array of BulkString is passed

--- app/resp.go
+++ app/resp.go
@@ -27,63 +27,61 @@ const (
 func Parse(s *bufio.Reader) ([]RESP, error) {
 	resps := []RESP{}
 
-	for {
-		rawLine, err := readLine(s)
+	rawLine, err := readLine(s)
+	if err != nil {
+		if err == io.EOF {
+			return nil, err
+		}
+		return nil, errors.New("Invalid RESP format. It does not end in CRLF.")
+	}
+
+	switch rawLine[0] {
+	case RESPArray:
+		count, err := strconv.Atoi(string(rawLine[1]))
 		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return nil, errors.New("Invalid RESP format. It does not end in CRLF.")
+			return nil, errors.New("Invalid RESP format (Array): Invalid integer.")
 		}
 
-		switch rawLine[0] {
-		case RESPArray:
-			count, err := strconv.Atoi(string(rawLine[1]))
-			if err != nil {
-				return nil, errors.New("Invalid RESP format (Array): Invalid integer.")
-			}
-
-			respArray := RESP{
-				Count: count,
-				Type:  RESPArray,
-			}
-
-			for i := 1; i <= count; i++ {
-				r, err := Parse(s)
-				if err != nil {
-					return nil, err
-				}
-				respArray.Array = append(respArray.Array, r...)
-			}
-			resps = append(resps, respArray)
-		case RESPBulkString:
-			count, err := strconv.Atoi(string(rawLine[1]))
-			if err != nil {
-				return nil, errors.New("Invalid RESP format (BulkString): Invalid integer.")
-			}
-
-			contentAndNewLine := make([]byte, count+len(newLine))
-			if _, err := io.ReadFull(s, contentAndNewLine); err != nil {
-				return nil, errors.New("Invalid RESP format (BulkString): Wrong content size.")
-			}
-			resps = append(resps, RESP{
-				Count: count,
-				Data:  contentAndNewLine[:len(contentAndNewLine)-len(newLine)],
-				Type:  RESPBulkString,
-			})
-		case RESPError:
-			panic("TODO")
-		case RESPInteger:
-			panic("TODO")
-		case RESPSimpleString:
-			resps = append(resps, RESP{
-				Count: -1,
-				Data:  rawLine[1 : len(rawLine)-len(newLine)],
-				Type:  RESPSimpleString,
-			})
-		default:
-			panic("TODO")
+		respArray := RESP{
+			Count: count,
+			Type:  RESPArray,
 		}
+
+		for i := 1; i <= count; i++ {
+			r, err := Parse(s)
+			if err != nil {
+				return nil, err
+			}
+			respArray.Array = append(respArray.Array, r...)
+		}
+		resps = append(resps, respArray)
+	case RESPBulkString:
+		count, err := strconv.Atoi(string(rawLine[1]))
+		if err != nil {
+			return nil, errors.New("Invalid RESP format (BulkString): Invalid integer.")
+		}
+
+		contentAndNewLine := make([]byte, count+len(newLine))
+		if _, err := io.ReadFull(s, contentAndNewLine); err != nil {
+			return nil, errors.New("Invalid RESP format (BulkString): Wrong content size.")
+		}
+		resps = append(resps, RESP{
+			Count: count,
+			Data:  contentAndNewLine[:len(contentAndNewLine)-len(newLine)],
+			Type:  RESPBulkString,
+		})
+	case RESPError:
+		panic("TODO")
+	case RESPInteger:
+		panic("TODO")
+	case RESPSimpleString:
+		resps = append(resps, RESP{
+			Count: -1,
+			Data:  rawLine[1 : len(rawLine)-len(newLine)],
+			Type:  RESPSimpleString,
+		})
+	default:
+		panic("TODO")
 	}
 
 	return resps, nil
